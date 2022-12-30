c5e374868b5742125c2f89cda7ac746639669fa0
Kamilah <k.muhammad95@gmail.com>
2022-12-30 11:42:22 -0600

    exercise 2 complete

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-           //EXERCISE 1
+            //EXERCISE 1
             var favNumber = 150;
             //var favNumber = r.Next(1, 200);
             Console.WriteLine("Enter a number between 1 and 200 to guess my fave number :)");
@@ -24,7 +24,33 @@
                 Console.WriteLine("Way to go! You've got it!");
             }
 
+            //EXERCISE 2
 
+            Console.WriteLine("What is your favorite school subject?");
+            var favSubject = Console.ReadLine();
+
+            switch (favSubject.ToLower())
+            {
+                case "english":
+                    Console.WriteLine("English is a fun subject!");
+                    break;
+                case  "math":
+                    Console.WriteLine("Math is great!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is my favorite subject too!");
+                    break;
+                case "social studies":
+                    Console.WriteLine("Social studies in interesting!");
+                    break;
+                case "history":
+                    Console.WriteLine("History is always fun to learn!");
+                    break;
+                default:
+                    Console.WriteLine("That's a cool subject too!");
+                    break;
+
+            }
         }
     }
 }
