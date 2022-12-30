7c5a8d293873f2d606df3b9df69fa6fb45e78217
Kamilah <k.muhammad95@gmail.com>
2022-12-30 11:05:47 -0600

    exercise 1 complete

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           //EXERCISE 1
+            var favNumber = 150;
+            //var favNumber = r.Next(1, 200);
+            Console.WriteLine("Enter a number between 1 and 200 to guess my fave number :)");
+            var userInput = int.Parse(Console.ReadLine());
+
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Good guess but not quite! Too low.");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Sorry buddy not quite. Too high.");
+            }
+            else
+            {
+                Console.WriteLine("Way to go! You've got it!");
+            }
+
+
         }
     }
 }
